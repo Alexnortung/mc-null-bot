e53ea8c056f7a9dedad2216850167b2f6705d963
Alexander Nortung <alex_nortung@live.dk>
2020-04-15 00:00:28 +0200

    started adding tests.

---
+++ test/test-state-machine.js
@@ -0,0 +1,9 @@
+require('should')
+
+describe('Statemachine' , function() {
+    describe('#pipeEvent', function() {
+        it('should send an event to the current state', function() {
+            (true).should.equal(true)
+        })
+    })
+})
